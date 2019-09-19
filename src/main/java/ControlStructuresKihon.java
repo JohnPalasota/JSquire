3f051c7b28a198ee474eaff8edeec210bcd07f48
John Palasota <John.Palasota@improving.com>
2019-09-19 09:14:52 -0500

    Run 1.a 9/19/2019

--- src/main/java/ControlStructuresKihon.java
+++ src/main/java/ControlStructuresKihon.java
@@ -6,38 +6,38 @@ import java.util.Collection;
 public class ControlStructuresKihon extends ControlStructuresKihonBase {
     @Override
     protected void callHitOnAIfValIsTrueElseCallHitOnB(boolean val, Target a, Target b) {
-        if(val){
+        if (val) {
             a.hit();
-        }else{
+        } else {
             b.hit();
         }
     }
 
     @Override
     protected void callHitOnAOnceForEachMemberOfList(Target a, Collection<String> list) {
-        for( String name : list ){
+        for (String name : list) {
             a.hit();
         }
     }
 
     @Override
     protected void callHitOnAWhileAIsValidIsTrue(Target a) {
-        while(a.getIsValid()){
+        while (a.getIsValid()) {
             a.hit();
         }
     }
 
     @Override
     protected void nTimesCallHitOnA(int n, Target a) {
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             a.hit();
         }
     }
 
     @Override
     protected void callHitOnAOnceAndLoopUntilIsValidIsFalse(Target a) {
-        do{
+        do {
             a.hit();
-        }while(a.getIsValid());
+        } while (a.getIsValid());
     }
 }
