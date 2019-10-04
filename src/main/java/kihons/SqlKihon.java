147d2a6f1c801e782d027f323cc6ba269b582ea3
John Palasota <John.Palasota@improving.com>
2019-10-04 08:11:44 -0500

    ...

--- src/main/java/kihons/SqlKihon.java
+++ src/main/java/kihons/SqlKihon.java
@@ -29,12 +29,12 @@ public class SqlKihon extends SqlKihonBase {
 
     @Override
     protected String Select_All_Fields_And_Rows_From_Person() {
-        return "SELECT * Person";
+        return "SELECT * FROM Person";
     }
 
     @Override
     protected String selectAllFieldsFromPersonJoinedToAddress() {
-        throw new NotImplementedYetException();
+        return "SELECT * FROM Person JOIN Person.PersonId = Address.PersonId";
     }
 
     @Override
